a74940b5b09a55877dcf30e68ef0aaab46186f3a
pmoniq <monika.pogocka@gmail.com>
2019-09-11 12:35:00 +0100

    Changed default value of DJANGO_SETTINGS_MODULE from task_app.settings to task_app.settings_staging.

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_app.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_app.settings_staging')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
